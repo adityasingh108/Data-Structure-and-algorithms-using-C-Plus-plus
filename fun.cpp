035f9685600e81a6ac9c6ac933f7e46d5c1dc11f
adityasingh108 <adityasingh78085@gmail.com>
2022-01-21 01:35:01 +0530

    recursion

--- fun.cpp
+++ fun.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 using namespace std; 
 
-// void sum(int a ,int b){
-//     int sum ;
-//     sum = a+b;
-//     cout << "the addition is :"<<sum<<endl;
-
-// }
 
 // default argument parameter 
 
@@ -14,11 +8,24 @@ int volume(int l = 1,int b =1,int h =1){
     return l*b*h;
 }
 
+// Function overloading
+
+void printNAme(int x){
+    cout<<"The intenger value "<< x<<endl;
+}
+void printNAme(float x){
+    cout<<"The float value "<< x<<endl;
+}
 
 
 int main()
 {
     // sum(4,6);
     
-    cout<<volume(5,5,5)<<endl;
+    // cout<<volume(5,5,5)<<endl;
+    int a = 45;
+    float b = 45.3;
+
+    printNAme(a);
+    printNAme(b);
 }
