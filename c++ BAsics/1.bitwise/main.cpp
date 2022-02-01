76d18bc1cfc771d995f4cdf7be268a7c89738208
adityasingh108 <adityasingh78085@gmail.com>
2022-02-01 14:02:58 +0530

    arrange the file

---
+++ c++ BAsics/1.bitwise/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+    int a = 5;
+    int b= 4;
+    // Bitwise opertaor 
+    cout<<"(a&B)"<< (a&b)<<endl;
+    cout<<"(a | B)"<< (a|b)<<endl;
+    cout<<"(a^B)"<< (a^b)<<endl;
+    cout<<"(~a)"<< (~a)<<endl;
+    cout<<"(~b)"<< (~b)<<endl;
+    return 0;
+}
