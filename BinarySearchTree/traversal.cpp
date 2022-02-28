06c23ccac854a04933120738272a2f32d15ec75d
adityasingh108 <adityasingh78085@gmail.com>
2022-02-28 10:47:43 +0530

    diagonal traversal

--- BinarySearchTree/traversal.cpp
+++ BinarySearchTree/traversal.cpp
@@ -172,4 +172,148 @@ vector<int> verticalOrder(Node *root)
         
     }
 
+vector<int> topView(Node *root)
+    {
+        // create a vector 
+        vector<int> ans;
+        
+        if(root== nullptr)
+            return ans;
+        
+        // create a map 
+        
+        map<int,int> topNode;
+        
+        //queue in which we stor the address of node and horzental value
+        queue<pair<Node*,int> > q;
+        q.push(make_pair(root,0));
+        
+        while(! q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            
+            Node *frontNode = temp.first;
+            
+            int hd = temp.second;
+            
+            if(topNode.find(hd) == topNode.end())
+                topNode[hd] = frontNode->data;
+                
+            if(frontNode->left)
+                q.push(make_pair(frontNode->left,hd-1));
+                
+            if(frontNode->right)
+                q.push(make_pair(frontNode->right,hd+1));    
+                
+            
+        }
+        
+        for(auto i:topNode){
+            ans.push_back(i.second);
+        }
+        return ans;
+        
+    }
 
+vector<int> rightView(Node *root){
+       vector<int> ans;
+       
+    //   create a queue and push the root in the queueu
+    queue<Node* > q;
+    q.push(root);
+    
+    while(!q.empty()){
+        
+        // count the number of the nodes in the  tree
+        int n = q.size();
+        
+        // traverse from the last element 
+        while(n--){
+            
+            Node *temp = q.front();
+            q.pop();
+            // if  n is last then push the element in the ans vector
+            
+            if(0 == n)
+                ans.push_back(temp->data);
+            
+            if(temp->left )
+                q.push(temp->left);
+                
+            if(temp->right )
+                q.push(temp->right); 
+            
+                
+            
+        }
+        
+    }
+    return ans;
+}    
+
+vector<int> leftView(Node *root){
+    vector<int> ans;
+    
+    if(!root)
+        return ans;
+        
+    // create queue ans push the root
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        // number of nodes in current level 
+        int n = q.size();
+        
+        // traverse all the nodes in the level
+        for(int i = 1; i<=n; i++){
+            
+            // creat a node that will store the front data
+            Node * temp = q.front();
+            q.pop();
+            
+            // print the data 
+            if( i == 1)
+                ans.push_back(temp->data);
+                
+            // if left root is not null push the node in the queue
+            if(temp->left)
+                q.push(temp->left);
+                
+            // if right root is not null push the node in the queue
+            if(temp->right)
+                q.push(temp->right);
+           
+        }
+    }
+  return ans;
+}
+
+vector<int> diagonal(Node *root)
+{
+     vector<int> ans;
+     
+     if(root == nullptr)
+        return ans;
+     
+     // create a queue
+     queue<Node*> q;
+     q.push(root);
+     
+     while(! q.empty()){
+         Node *temp = q.front();
+         q.pop();
+         
+         while(temp){
+             
+             if(temp->left)
+                q.push(temp->left);
+            
+            ans.push_back(temp->data);   
+            temp = temp->right;
+         }
+         
+         
+     }
+     return ans;
+}
